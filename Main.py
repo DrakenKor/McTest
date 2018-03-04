4c1ee605975bcfb46b456fa7aec5caf359620507
drakenkor <Github2trust?>
2018-03-04 16:41:17 +0530

    Test commit

---
+++ Main.py
@@ -0,0 +1,20 @@
+import sys
+import os
+print('hello')
+print('line2')
+print('line3')
+x = 3
+print('line4')
+print('line5')
+y = 4  # hello
+"""
+hello
+hehe
+comments are cool
+multiline comments are the best
+"""
+# hello
+"""
+comments again
+"""
+os._exit()
